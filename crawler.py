679581d256e4ae260c8030288b0d39bafb6ff056
Mingqing Chen <mingqingchen@gmail.com>
2018-04-27 02:29:55 -0700

    Updated TD crawler, with dynamic token and json parser

--- crawler.py
+++ crawler.py
@@ -16,14 +16,14 @@ import requests
 import tensorflow as tf
 
 import os, sys
-
+import json
 import util.datetime_util as datetime_util
 import proto.stock_pb2 as stock_pb2
 
 import pdb
 
 k_intra_day_folder = 'intra_day/'
-k_data_folder = './data/'
+k_data_folder = './data_test/'
 k_index_list = {'NASDAQ', 'NYSE', 'AMEX'}
 
 class IntraDayCrawler:
@@ -150,76 +150,102 @@ class IntraDayCrawler:
 class IntraDayCrawlerTD(IntraDayCrawler):
   def __init__(self, data_folder):
     IntraDayCrawler.__init__(self, data_folder)
-    self.query_template_ = 'https://api.tdameritrade.com/v1/marketdata/{0}/pricehistory?period=1&frequencyType=minute&frequency=1'    
-    self.__load_token()
-    self.has_token_ = self.__load_token()
-
-  def __load_token(self):
-    k_token_file = './token.txt'
-    if not os.path.isfile(k_token_file):
-      print('Token file does not exist')
-      return False
-    fid = open('token.txt')
-    lines = fid.readlines()
-    fid.close()
-
-    self.token_string_ = lines[0]
-    self.token_string_ = self.token_string_.replace('\n', '')
-    return True
-
-  def crawl_one_symbol(self, symbol):
-    local_file_name = './temp.txt'
-    query_string = self.token_string_ + ' "' + self.query_template_.format(symbol) + '" > {0}'.format(local_file_name)
-    pdb.set_trace()
-    os.system(query_string)
-
-    fid = open(local_file_name)
-    lines = fid.readlines()
-    fid.close()
-
-    one_symbol_data = stock_pb2.OneIntraDayData()
-    one_symbol_data.symbol = symbol
-    one_symbol_data.date = datetime_util.get_today()
-    one_symbol_data.resolution = 1
-    for line in lines:
-      if '"candles":' in line:
-        start_index = line.index('[')
-        finish_index = line.index(']')
-        line = line[start_index + 1 : finish_index]
-        split_lines = line.split('},')   
     
-        for timeslot_line in split_lines:
-          timeslot_line = timeslot_line.replace('{', '')
-          timeslot_line = timeslot_line.replace('}', '')
-          split_content = timeslot_line.split(',')
-      
-          one_time_data = one_symbol_data.data.add()
-          for content in split_content:
-            kv_pair = content.split(':')
-            if len(kv_pair)!=2:
-              continue
-            str_key = kv_pair[0].replace('"', '')
-            str_val = kv_pair[1]
-            if str_key == 'open':
-              one_time_data.open = float(str_val)
-            elif str_key == 'high':
-              one_time_data.high = float(str_val)
-            elif str_key == 'close':
-              one_time_data.close = float(str_val)
-            elif str_key == 'low':
-              one_time_data.low = float(str_val)
-            elif str_key == 'volume':
-              one_time_data.volume = int(str_val)
-            elif str_key == 'datetime':
-              cl_time = pd.to_datetime(int(str_val), unit = 'ms')
-              cl_time = cl_time.tz_localize('UTC').tz_convert('America/Los_Angeles')
-              one_time_data.time_val = cl_time.hour * 100 + cl_time.minute
+    self.get_access_token_template_ = 'curl -X POST --header "Content-Type: application/x-www-form-urlencoded" -d "grant_type=refresh_token&refresh_token={0}&access_type=offline&code=&client_id=mingqing8%40AMER.OAUTHAP&redirect_uri=sunrsie" "https://api.tdameritrade.com/v1/oauth2/token" > {1}'
+    self.get_history_price_template_ = 'curl -X GET --header "Authorization: " --header "Authorization: Bearer {0}" "https://api.tdameritrade.com/v1/marketdata/{1}/pricehistory?period=1&frequencyType=minute&frequency=1" > {2}'
+    
+    self.temp_file_location_ = 'temp.txt'
+    self.refresh_token_file_ = './refresh_token.txt'
+    self.__get_refresh_token()
+    self.get_new_access_token()
+  
 
-    return True, one_symbol_data
+  def __get_refresh_token(self):
+    fid = open(self.refresh_token_file_)
+    lines = fid.readlines()
+    fid.close()
+    self.refresh_token_ = urllib.quote_plus(lines[0].replace('\n', ''))
+
+  def get_new_access_token(self):
+    get_access_token_command = self.get_access_token_template_.format(self.refresh_token_, self.temp_file_location_)
+    os.system(get_access_token_command)
+    print get_access_token_command
+    response = json.load(open(self.temp_file_location_))
+    if 'error' in response:
+      print ('Could not get access token. Something is wrong !!!')
+      return False
+    else:
+      self.refresh_token_ = response['refresh_token']
+      self.access_token_ = response['access_token']
+      fid = open(self.refresh_token_file_, 'w')
+      fid.write(self.refresh_token_)
+      fid.close()
+      print('Successfully Update Refresh and Access Token!')
+      return True
+
+  def __query_once(self, symbol):
+    query_string = self.get_history_price_template_.format(self.access_token_, symbol, self.temp_file_location_)
+    os.system(query_string)
+    response = dict()
+    try:
+      response = json.load(open(self.temp_file_location_))
+    except ValueError:
+      print('Error decoding json.')
+      return False, response
+    return True, response
+  
+  def crawl_one_symbol(self, symbol):
+    result, response = self.__query_once(symbol)
+    
+    # For the first time, it could be due to expired access token.
+    if 'error' in response or (not result):
+      self.get_new_access_token()
+      result, response = self.__query_once(symbol)
+    
+    one_symbol_data = stock_pb2.OneIntraDayData()
+    if 'error' in response or (not result):
+      print ('Could not query symbol {0}. Something is wrong !!!'.format(symbol))
+      return False, one_symbol_data
+    elif 'candles' not in response:
+      print ('Could not find candles in response of symbol {0}. Something is wrong !!!'.format(symbol))
+      return False, one_symbol_data
+    else:
+      one_symbol_data.symbol = symbol
+      one_symbol_data.date = datetime_util.get_today()
+      one_symbol_data.resolution = 1
+    
+      for one_time_data_js in response['candles']:
+        if 'open' not in one_time_data_js:
+          continue
+        if 'close' not in one_time_data_js:
+          continue
+        if 'volume' not in one_time_data_js:
+          continue
+        if 'high' not in one_time_data_js:
+          continue
+        if 'low' not in one_time_data_js:
+          continue
+        if 'datetime' not in one_time_data_js:
+          continue
+        one_time_data = one_symbol_data.data.add()
+        one_time_data.open = float(one_time_data_js['open'])
+        one_time_data.close = float(one_time_data_js['close'])
+        one_time_data.high = float(one_time_data_js['high'])
+        one_time_data.low = float(one_time_data_js['low'])
+        one_time_data.volume = int(one_time_data_js['volume'])
+        
+        cl_time = pd.to_datetime(int(one_time_data_js['datetime']), unit = 'ms')
+        cl_time = cl_time.tz_localize('UTC').tz_convert('America/Los_Angeles')
+        one_time_data.time_val = cl_time.hour * 100 + cl_time.minute
+    if len(one_symbol_data.data) < self.min_num_time_slot_threshold_:
+      print('Crawled time slot is too few. Data is considered not useful.')
+      return False, one_symbol_data
+    else:
+      return True, one_symbol_data
 
 def main(_):
-  # crawler = IntraDayCrawlerTD(FLAGS.data_folder)
-  crawler = IntraDayCrawler(FLAGS.data_folder)
+  crawler = IntraDayCrawlerTD(FLAGS.data_folder)
+  # crawler = IntraDayCrawler(FLAGS.data_folder)
   crawler.daily_crawl()
 
 if __name__=='__main__':
