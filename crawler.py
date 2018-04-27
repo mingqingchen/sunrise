72c9ffd14b33127f3c538b82e8757b23245f9f47
Mingqing Chen <mingqing@mingqing.mtv.corp.google.com>
2018-04-26 17:42:51 -0700

    add crawler for td ameritrade

--- crawler.py
+++ crawler.py
@@ -31,10 +31,10 @@ class IntraDayCrawler:
     self.data_folder_ = data_folder
     self.overwrite_ = False
     self.index_list_url_template = 'https://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange={0}&render=download'
-    self.min_num_time_slot_threshold_ = 50
+    self.min_num_time_slot_threshold_ = 1
     
     
-  def get_google_finance_intraday(self, ticker, period=60, days=1):
+  def crawl_one_symbol(self, ticker):
     """
     Retrieve intraday stock data from Google Finance.
     Parameters
@@ -53,6 +53,8 @@ class IntraDayCrawler:
         the retrieved price values.
     """
 
+    period = 1
+    days = 1
     uri = 'http://www.google.com/finance/getprices' \
           '?i={period}&p={days}d&f=d,o,h,l,c,v&df=cpct&q={ticker}'.format(ticker=ticker,
                                                                           period=period,
@@ -93,13 +95,13 @@ class IntraDayCrawler:
       return False, one_day_data
     return True, one_day_data
 
-  def crawl_one_symbol(self, symbol):
+  def crawl_and_export_one_symbol(self, symbol):
     output_file_path = os.path.join(self.today_folder_, symbol + '.pb')
     if (not self.overwrite_) and os.path.isfile(output_file_path):
       print('Symbol {0} already crawled. Skip.'.format(symbol))
       return
     print('Crawling: {0}'.format(symbol))
-    result, one_day_data = self.get_google_finance_intraday(symbol, period = 2, days = 1)
+    result, one_day_data = self.crawl_one_symbol(symbol)
     
     if result:
       fid = open(output_file_path, 'w')
@@ -143,9 +145,80 @@ class IntraDayCrawler:
         if '^' in symbol:
           continue
         symbol = symbol.replace(' ', '')
-        self.crawl_one_symbol(symbol)
+        self.crawl_and_export_one_symbol(symbol)
+
+class IntraDayCrawlerTD(IntraDayCrawler):
+  def __init__(self, data_folder):
+    IntraDayCrawler.__init__(self, data_folder)
+    self.query_template_ = 'https://api.tdameritrade.com/v1/marketdata/{0}/pricehistory?period=1&frequencyType=minute&frequency=1'    
+    self.__load_token()
+    self.has_token_ = self.__load_token()
+
+  def __load_token(self):
+    k_token_file = './token.txt'
+    if not os.path.isfile(k_token_file):
+      print('Token file does not exist')
+      return False
+    fid = open('token.txt')
+    lines = fid.readlines()
+    fid.close()
+
+    self.token_string_ = lines[0]
+    self.token_string_ = self.token_string_.replace('\n', '')
+    return True
+
+  def crawl_one_symbol(self, symbol):
+    local_file_name = './temp.txt'
+    query_string = self.token_string_ + ' "' + self.query_template_.format(symbol) + '" > {0}'.format(local_file_name)
+    pdb.set_trace()
+    os.system(query_string)
+
+    fid = open(local_file_name)
+    lines = fid.readlines()
+    fid.close()
+
+    one_symbol_data = stock_pb2.OneIntraDayData()
+    one_symbol_data.symbol = symbol
+    one_symbol_data.date = datetime_util.get_today()
+    one_symbol_data.resolution = 1
+    for line in lines:
+      if '"candles":' in line:
+        start_index = line.index('[')
+        finish_index = line.index(']')
+        line = line[start_index + 1 : finish_index]
+        split_lines = line.split('},')   
+    
+        for timeslot_line in split_lines:
+          timeslot_line = timeslot_line.replace('{', '')
+          timeslot_line = timeslot_line.replace('}', '')
+          split_content = timeslot_line.split(',')
+      
+          one_time_data = one_symbol_data.data.add()
+          for content in split_content:
+            kv_pair = content.split(':')
+            if len(kv_pair)!=2:
+              continue
+            str_key = kv_pair[0].replace('"', '')
+            str_val = kv_pair[1]
+            if str_key == 'open':
+              one_time_data.open = float(str_val)
+            elif str_key == 'high':
+              one_time_data.high = float(str_val)
+            elif str_key == 'close':
+              one_time_data.close = float(str_val)
+            elif str_key == 'low':
+              one_time_data.low = float(str_val)
+            elif str_key == 'volume':
+              one_time_data.volume = int(str_val)
+            elif str_key == 'datetime':
+              cl_time = pd.to_datetime(int(str_val), unit = 'ms')
+              cl_time = cl_time.tz_localize('UTC').tz_convert('America/Los_Angeles')
+              one_time_data.time_val = cl_time.hour * 100 + cl_time.minute
+
+    return True, one_symbol_data
 
 def main(_):
+  # crawler = IntraDayCrawlerTD(FLAGS.data_folder)
   crawler = IntraDayCrawler(FLAGS.data_folder)
   crawler.daily_crawl()
 
