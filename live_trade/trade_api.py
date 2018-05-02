80cac5ff2e58bdd473097379d5aeaa4606919afd
Q Li <qianwenl@zillowgroup.com>
2018-05-01 21:05:45 -0700

    refactor to use urllib3

--- live_trade/trade_api.py
+++ live_trade/trade_api.py
@@ -1,8 +1,6 @@
 import urllib
-import os
 import json
 import urllib3
-import time
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -12,12 +10,13 @@ class TradeAPI:
   def __init__(self):
     self.refresh_token_file_ = './refresh_token.txt'
     self.access_token_file_ = './access_token.txt'
-    self.temp_file_location_ = 'temp.txt'
 
-    self.get_access_token_template_ = 'curl -X POST --header "Content-Type: application/x-www-form-urlencoded" -d "grant_type=refresh_token&refresh_token={0}&access_type=offline&code=&client_id=mingqing8%40AMER.OAUTHAP&redirect_uri=sunrsie" "https://api.tdameritrade.com/v1/oauth2/token" > {1}'
-    self.get_history_price_template_ = 'curl -X GET --header "Authorization: " --header "Authorization: Bearer {0}" "https://api.tdameritrade.com/v1/marketdata/{1}/pricehistory?period=1&frequencyType=minute&frequency=1" > {2}'
+    self.get_access_token_url = 'https://api.tdameritrade.com/v1/oauth2/token'
+    self.get_access_token_body = 'grant_type=refresh_token&refresh_token={0}&access_type=offline&code=&client_id=mingqing8%40AMER.OAUTHAP&redirect_uri=sunrsie'
+
+    self.get_history_price_template_ = 'https://api.tdameritrade.com/v1/marketdata/{0}/pricehistory?period=1&frequencyType=minute&frequency=1'
     self.real_time_quotes_template_ = 'https://api.tdameritrade.com/v1/marketdata/quotes?apikey={0}&symbol={1}'
-    
+
     self.http = urllib3.PoolManager()
 
   def get_refresh_token(self):
@@ -27,9 +26,13 @@ class TradeAPI:
     self.refresh_token_ = urllib.quote_plus(lines[0].replace('\n', ''))
 
   def get_new_access_token(self):
-    get_access_token_command = self.get_access_token_template_.format(self.refresh_token_, self.temp_file_location_)
-    os.system(get_access_token_command)
-    response = json.load(open(self.temp_file_location_))
+    query_body = self.get_access_token_body.format(self.refresh_token_)
+    query_response = self.http.request('POST',
+                                       self.get_access_token_url,
+                                       headers={'Content-Type': 'application/x-www-form-urlencoded'},
+                                       body=query_body)
+    response = json.loads(query_response.data.decode('utf-8'))
+
     if 'error' in response:
       print ('Could not get access token. Something is wrong !!!')
       return False
@@ -48,11 +51,11 @@ class TradeAPI:
 
   def query_historical_price(self, symbol):
     """ Get historical price of a symbol. Large time delay. Typically only able to query after market close for a day. """
-    query_string = self.get_history_price_template_.format(self.access_token_, symbol, self.temp_file_location_)
-    os.system(query_string)
-    response = dict()
+    query_request = self.get_history_price_template_.format(symbol)
+    query_header_authorization = 'Bearer {0}'.format(self.access_token_)
+    query_response = self.http.request('GET', query_request, headers={'Authorization': query_header_authorization})
     try:
-      response = json.load(open(self.temp_file_location_))
+      response = json.loads(query_response.data.decode('utf-8'))
     except ValueError:
       print('Error decoding json.')
       return False, response
