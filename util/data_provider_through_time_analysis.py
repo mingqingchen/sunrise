43f16769d005bb9b833162d6e6c84ff7a02d068f
Mingqing Chen <mingqing@mingqing-macbookpro.roam.corp.google.com>
2019-01-18 22:04:47 -0800

    by symbol analysis of current crawled data

--- util/data_provider_through_time_analysis.py
+++ util/data_provider_through_time_analysis.py
@@ -1,4 +1,5 @@
 import argparse
+import operator
 import sys
 import tensorflow as tf
 import data_provider as data_provider
@@ -7,21 +8,39 @@ k_data_folder = './data/minute_data/'
 
 def analyze_data_quality_through_time(argv):
   """Analyze crawled data through time. Print stats for number of symbols that have more than 50 data points. """
-  start_date = 20190101
-  end_date = 20190130
+  start_date = 20180101
+  end_date = 20190131
+  start_date_dict, end_date_dict, count_dict, valid_count_dict = {}, {}, {}, {}
+
   dp = data_provider.DataProvider(FLAGS.data_folder, False)
   all_sub_folders = dp.get_all_available_dates()
-  for sub_folder in all_sub_folders:
-    if int(sub_folder) < start_date or int(sub_folder) > end_date:
+  for str_date_val in all_sub_folders:
+    date_val = int(str_date_val)
+    if date_val < start_date or date_val > end_date:
       continue
-    dp.load_one_day_data(sub_folder)
-    one_day_list = dp.get_symbol_list_for_a_day(sub_folder)
+    dp.load_one_day_data(str_date_val)
+    one_day_list = dp.get_symbol_list_for_a_day(str_date_val)
     num_valid = 0
     for symbol in one_day_list:
       one_symbol_data = dp.get_one_symbol_data(symbol)
+      if symbol not in start_date_dict:
+        start_date_dict[symbol] = date_val
+        end_date_dict[symbol] = date_val
+        count_dict[symbol] = 1
+        valid_count_dict[symbol] = 0
       if len(one_symbol_data.data) > 20:
         num_valid += 1
-    print('Date: %s, number of symbols: %d, valid ones: %d' % (sub_folder, len(one_day_list), num_valid))
+        valid_count_dict[symbol] += 1
+      count_dict[symbol] += 1
+      start_date_dict[symbol] = min(date_val, start_date_dict[symbol])
+      end_date_dict[symbol] = max(date_val, end_date_dict[symbol])
+    print('Date: %s, number of symbols: %d, valid ones: %d' % (str_date_val, len(one_day_list), num_valid))
+
+  for iter in sorted(count_dict.items(), key=operator.itemgetter(1)):
+    symbol = iter[0]
+    print('Symbol: %s\tStart Date: %d\tEnd Date: %d\tNumber: %d\tValid number: %d' % (symbol, start_date_dict[symbol],
+        end_date_dict[symbol], count_dict[symbol], valid_count_dict[symbol]))
+
 
 
 if __name__ == "__main__":
